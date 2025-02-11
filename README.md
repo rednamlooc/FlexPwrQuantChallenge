33bd2e7a9e864ca8056c750f531e0e79a51f2a4e
rednamlooc <lasche02@gmail.com>
2025-02-11 17:14:17 +0000

    finished all the tasks

--- README.md
+++ README.md
@@ -26,12 +26,14 @@ Total PV forcasteded Day Ahead:  46094572.75
 Total PV forcasteded Intraday:  46689891.5
 
 ### Task 2.2
-![average Wind/Solar production 2021](average_forecast.png)
+![average Wind/Solar production 2021](pics/average_forecast.png)
 
 ### Task 2.3
 Average Wind Value [in EUR/MWh]:  81.38706977035753
 
 Average PV Value [in EUR/MWh]:  76.89453079034125
+
+Average Day Ahead Price [in EUR/MWh]:  96.84991780821919
 
 The average value of Wind and PV is lower than the average day ahead price. I can think of some reasons for this: 1. there might be forcasting inaccuracies, that affect the price 2. the average day ahead price is calculated over all hours, while the average value of Wind and PV is calculated over the forecasted production (being a volume weighted average) 3. there might be issues with oversupply during certain hours. To asses these closer, I would need to look at the data in more detail.
 
@@ -63,3 +65,40 @@ My idea here is to look at the Intraday Quarterhourly Price markets (aussuming t
 If we actually used this strategy, the battery would have generated 25831.525 EUR in 2021 (ignoring some things like trading fees, energy loss and so on).
 
 ### Task 2.7
+![correlation matrix](pics/correlation_matrix.png)
+
+What we are interested in, is the difference between the day ahead and the intraday hourly prices. We have excess to the day ahead data (pv and wind forecasts as well as the day ahead prices) and then we need to make decisions on whether to buy energy to those prices.
+
+![difference da - id over time](pics/difference_da_id_over_time.png)
+
+The winter looks to have much more volatility between the intraday price vs the day ahead price. Although we can see some big spikes over the whole year for the spread in both directions. 
+
+![average spread](pics/average_spread.png)
+
+![average spread by hour and month](pics/average_spread_by_hour_month.png)
+
+One idea would be to just buy 17 and 18 o'clock every day. This seems to be, an average, the only time, where we get a positive spread between day ahead and the intraday prices. When we consider the hours, by each month, we can see the pattern that in certains months (december, october, july, may, march as well as january and february) have blocks of hours where the average spread is positive. But it would be hard to justify, just from this data, why these months should be chosen (it isn't claer that this strategy would also work for example for 2022 and we can't overfit a strategy on old data). 
+
+![average spread by hour and weekday](pics/average_spread_by_hour_weekday.png)
+
+Maybe we can see that there are some inefficiencies when it comes to the demand on the weekends (fridays, saturdays and sundays) where the dayahead price underestimates the demand in the evenings.
+
+I tried to find a way to implement the forecasted values for wind and pv. The correlation with the day ahead PV forecast is positive, but with the day ahead wind forecast is actually negative. There is probably a more complex algorithm to explore a strategy here that uses intelligent forecast signals. In the restricted time I had for this excercise, I couldn't come up with one that seemed to work (without overfitting to my testing data in a way that doesn't seem logical to me). 
+
+So I tried two strategies: 
+
+1.Buying and selling 17 and 18 o'clock every day.
+
+![cumulative profit strategy 1](pics/cumulative_strategy_1_profit.png)
+
+Here we assume that we always buy and sell 100 MW (this skips over questions of loosing part of our capital and so on). This strategy would actually make a profit of 66000 EUR over the year. As we can see in the graph though it has quite a lot of volatility. We could probably do better by adding indicators for how big to size our position. This strategy is probably not very good in the real world. We are trying very hard to fit our data and we don't account for any trading fees and so on. 
+
+2.Buying and selling fridays, saturdays and sundays from 15 to 21 o'clock.
+
+![cumulative profit strategy 2](pics/cumulative_strategy_2_profit.png)
+
+We still assume we always risk our entire portfolio on a sigles trade (obv. this is not a good idea due to risk management). This strategy gives us a more reliable profit curve and (probably) exploits misspricing of energy on the evening of "weekend days", where demand is expected to be lower, but it seems like more often then not it turns out to be higher. This strategy gives a profit of 376906 EUR for the year. Excluding fees, taxes and so on.
+
+As described above the stragies are very rudimentary, we obviously need to implmenet some form of risk managment (this just assumes that we use the whole portfolio to trade this strategy, because we have a number of portfolios doing different things). This would include the sizing of our position optimally including indicators like the forecasted wind and pv production.
+
+I hope, I was able to show some of my coding skills. I have to say that I am not too happy with how Task 2.7 turned out, as I would have liked to find a better trading strategy. But this is what I have been able to come up with in the short timeframe, that I was given. Additionally more knowledge about the energy market would be very helpful to be able top draw connections quicker and come up with more interesting hypothesis. 
--- task2.py
+++ task2.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load data
 df = pd.read_excel("analysis_task_data.xlsx")
@@ -14,16 +15,16 @@ print(df.columns)
 # Task 2.1: 
 # How much Wind/PV Power was forecasted to produced in German in 2021 [in MWh] on Day Ahead (da) and on Intraday (id).
 
-df['Wind Day Ahead Forecast [in MWh]'] = df['Wind Day Ahead Forecast [in MW]'] * 0.25
-df['PV Day Ahead Forecast [in MWh]'] = df['PV Day Ahead Forecast [in MW]'] * 0.25
-df['Wind Intraday Forecast [in MWh]'] = df['Wind Intraday Forecast [in MW]'] * 0.25
-df['PV Intraday Forecast [in MWh]'] = df['PV Intraday Forecast [in MW]'] * 0.25
+df["Wind Day Ahead Forecast [in MWh]"] = df["Wind Day Ahead Forecast [in MW]"] * 0.25
+df["PV Day Ahead Forecast [in MWh]"] = df["PV Day Ahead Forecast [in MW]"] * 0.25
+df["Wind Intraday Forecast [in MWh]"] = df["Wind Intraday Forecast [in MW]"] * 0.25
+df["PV Intraday Forecast [in MWh]"] = df["PV Intraday Forecast [in MW]"] * 0.25
 
 # as the data runs from 2021-01-01 to 2021-12-31, we can just sum the MWh columns
-wind_da_total = df['Wind Day Ahead Forecast [in MWh]'].sum()
-wind_id_total = df['Wind Intraday Forecast [in MWh]'].sum()
-pv_da_total = df['PV Day Ahead Forecast [in MWh]'].sum()
-pv_id_total = df['PV Intraday Forecast [in MWh]'].sum()
+wind_da_total = df["Wind Day Ahead Forecast [in MWh]"].sum()
+wind_id_total = df["Wind Intraday Forecast [in MWh]"].sum()
+pv_da_total = df["PV Day Ahead Forecast [in MWh]"].sum()
+pv_id_total = df["PV Intraday Forecast [in MWh]"].sum()
 
 
 print("Total Wind forcasteded Day Ahead: ", wind_da_total)
@@ -36,31 +37,31 @@ print("PV da - id: ", pv_da_total - pv_id_total)
 # Task 2.2: Show the average Wind/Solar production for 2021 over a 24h period for Intraday and Day Ahead (4 lines in one graph).
 
 # average wind and pv production per hour
-avg_wind_da_per_hour = df.groupby('hour')['Wind Day Ahead Forecast [in MWh]'].mean()
-avg_pv_da_per_hour = df.groupby('hour')['PV Day Ahead Forecast [in MWh]'].mean()
-avg_wind_id_per_hour = df.groupby('hour')['Wind Intraday Forecast [in MWh]'].mean()
-avg_pv_id_per_hour = df.groupby('hour')['PV Intraday Forecast [in MWh]'].mean()
+avg_wind_da_per_hour = df.groupby("hour")["Wind Day Ahead Forecast [in MWh]"].mean()
+avg_pv_da_per_hour = df.groupby("hour")["PV Day Ahead Forecast [in MWh]"].mean()
+avg_wind_id_per_hour = df.groupby("hour")["Wind Intraday Forecast [in MWh]"].mean()
+avg_pv_id_per_hour = df.groupby("hour")["PV Intraday Forecast [in MWh]"].mean()
 
 plt.figure(figsize=(12, 6))
-plt.plot(avg_wind_da_per_hour, label='Wind Day Ahead Forecast [in MWh]')
-plt.plot(avg_pv_da_per_hour, label='PV Day Ahead Forecast [in MWh]')
-plt.plot(avg_wind_id_per_hour, label='Wind Intraday Forecast [in MWh]')
-plt.plot(avg_pv_id_per_hour, label='PV Intraday Forecast [in MWh]')
-plt.xlabel('Hour of the Day')
-plt.ylabel('Average Forecast [in MWh]')
-plt.title('Average Wind/PV Forecast for 2021 Over a 24h Period')
+plt.plot(avg_wind_da_per_hour, label="Wind Day Ahead Forecast [in MWh]")
+plt.plot(avg_pv_da_per_hour, label="PV Day Ahead Forecast [in MWh]")
+plt.plot(avg_wind_id_per_hour, label="Wind Intraday Forecast [in MWh]")
+plt.plot(avg_pv_id_per_hour, label="PV Intraday Forecast [in MWh]")
+plt.xlabel("Hour of the Day")
+plt.ylabel("Average Forecast [in MWh]")
+plt.title("Average Wind/PV Forecast for 2021 Over a 24h Period")
 plt.legend()
 plt.show()
-plt.savefig('average_forecast.png')
+plt.savefig("pics/average_forecast.png")
 
 # Task 2.3: What was the average value [in EUR/MWh] for Wind/Solar Power in 2021 using the da forecast and using da h prices? Is the average value of Wind and PV higher or lower than the average da price? Why could it be higher/lower?
 
-df['Wind Revenue [in EUR]'] = df['Wind Day Ahead Forecast [in MWh]'] * df['Day Ahead Price hourly [in EUR/MWh]']
-df['PV Revenue [in EUR]'] = df['PV Day Ahead Forecast [in MWh]'] * df['Day Ahead Price hourly [in EUR/MWh]']
+df["Wind Revenue [in EUR]"] = df["Wind Day Ahead Forecast [in MWh]"] * df["Day Ahead Price hourly [in EUR/MWh]"]
+df["PV Revenue [in EUR]"] = df["PV Day Ahead Forecast [in MWh]"] * df["Day Ahead Price hourly [in EUR/MWh]"]
 
 # Calculate the total value and total forecasted production
-total_wind_revenue = df['Wind Revenue [in EUR]'].sum()
-total_pv_revenue = df['PV Revenue [in EUR]'].sum()
+total_wind_revenue = df["Wind Revenue [in EUR]"].sum()
+total_pv_revenue = df["PV Revenue [in EUR]"].sum()
 
 
 # Calculate the average value in EUR/MWh
@@ -68,7 +69,7 @@ avg_wind_value_per_mwh = total_wind_revenue / wind_da_total
 avg_pv_value_per_mwh = total_pv_revenue / pv_da_total
 
 # Calculate the average day ahead price
-avg_da_price = df['Day Ahead Price hourly [in EUR/MWh]'].mean()
+avg_da_price = df["Day Ahead Price hourly [in EUR/MWh]"].mean()
 
 print("Average Wind Value [in EUR/MWh]: ", avg_wind_value_per_mwh)
 print("Average PV Value [in EUR/MWh]: ", avg_pv_value_per_mwh)
@@ -79,7 +80,7 @@ print("The average value of Wind and PV is lower than the average day ahead pric
 # Task 2.4: Find the Day with the highest renewable energy production and with the lowest renewable energy production in 2021. What was the average Day Ahead Price levels on these days? How do you explain the difference in prices?
 
 # Calculate the total renewable energy production per day
-df['Total Renewable Forecast [in MWh]'] = df['Wind Day Ahead Forecast [in MWh]'] + df['PV Day Ahead Forecast [in MWh]']
+df["Total Renewable Forecast [in MWh]"] = df["Wind Day Ahead Forecast [in MWh]"] + df["PV Day Ahead Forecast [in MWh]"]
 
 daily_df = df.groupby(df["time"].dt.date).agg(
     total_renewable=("Total Renewable Forecast [in MWh]", "sum"),
@@ -142,18 +143,142 @@ highest_price = highest_price_row["avg_price"]
 print("Lowest average price per quarter-hour: ", lowest_price, " at hour: ", lowest_price_row["hour"], " and minute: ", lowest_price_row["minute"])
 print("Highest average price per quarter-hour: ", highest_price, " at hour: ", highest_price_row["hour"], " and minute: ", highest_price_row["minute"])
 
-print("We try out the strategy of buying our anergy at 4 o'clock and selling it at 20 o'clock")
+print("We try out the strategy of buying our energy at 4 o'clock and selling it at 20 o'clock")
 
 # Calculate the revenue generated by the battery for each day
-df['date'] = df['time'].dt.date
-daily_revenue = df.groupby('date').apply(
-    lambda x: x[(x['hour'] == 20) & (x['minute'] == 0)]['Intraday Price Price Quarter Hourly  [in EUR/MWh]'].values[0] -
-              x[(x['hour'] == 4) & (x['minute'] == 0)]['Intraday Price Price Quarter Hourly  [in EUR/MWh]'].values[0]
+df["date"] = df["time"].dt.date
+daily_revenue = df.groupby("date").apply(
+    lambda x: x[(x["hour"] == 20) & (x["minute"] == 0)]["Intraday Price Price Quarter Hourly  [in EUR/MWh]"].values[0] -
+              x[(x["hour"] == 4) & (x["minute"] == 0)]["Intraday Price Price Quarter Hourly  [in EUR/MWh]"].values[0]
 ).sum()
 
 print("Total revenue generated by the battery in 2021: ", daily_revenue)
 
 # Task 2.7: Come up with a trading strategy that makes money between the day ahead hourly prices and the intraday hourly prices.
 
+df["difference da - id"] = df["Day Ahead Price hourly [in EUR/MWh]"] - df["Intraday Price Hourly  [in EUR/MWh]"]
+
+# Select relevant columns for correlation matrix
+correlation_columns = [
+    "difference da - id",
+    "Day Ahead Price hourly [in EUR/MWh]",
+    "Intraday Price Hourly  [in EUR/MWh]",
+    "PV Day Ahead Forecast [in MWh]",
+    "Wind Day Ahead Forecast [in MWh]",
+    "PV Intraday Forecast [in MWh]",
+    "Wind Intraday Forecast [in MWh]"
+]
+
+# Calculate correlation matrix
+correlation_matrix = df[correlation_columns].corr()
+
+print(correlation_matrix)
+
+# Plot correlation matrix
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
+plt.title("Correlation Matrix")
+plt.savefig("pics/correlation_matrix.png", bbox_inches="tight")
+plt.show()
 
 
+# Plot the difference between Day Ahead and Intraday prices over time
+plt.figure(figsize=(12, 6))
+plt.plot(df["time"], df["difference da - id"], label="Difference DA - ID [in EUR/MWh]")
+plt.xlabel("Time")
+plt.ylabel("Difference [in EUR/MWh]")
+plt.title("Difference between Day Ahead and Intraday Prices Over Time")
+plt.legend()
+plt.savefig("pics/difference_da_id_over_time.png", bbox_inches="tight")
+plt.show()
+
+df["weekday"] = df["time"].dt.weekday
+df["weekend"] = df["time"].dt.weekday.isin([5, 6])
+df["month"] = df["time"].dt.month
+
+avg_spread_by_hour = df.groupby("hour")["difference da - id"].mean()
+avg_spread_by_weekday = df.groupby("weekday")["difference da - id"].mean()
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+avg_spread_by_hour.plot(kind="bar", title="Average Spread by Hour of Day", xlabel="Hour", ylabel="Spread [EUR/MWh]")
+plt.subplot(1, 2, 2)
+avg_spread_by_weekday.plot(kind="bar", title="Average Spread by Weekday", xlabel="Weekday", ylabel="Spread [EUR/MWh]")
+plt.tight_layout()
+plt.show()
+plt.savefig("pics/average_spread.png", bbox_inches="tight")
+
+# Calculate the average spread per hour for each month
+avg_spread_by_hour_month = df.groupby(["month", "hour"])["difference da - id"].mean().unstack()
+
+# Plot the average spread per hour for each month
+plt.figure(figsize=(15, 10))
+sns.heatmap(avg_spread_by_hour_month, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
+plt.title("Average Spread by Hour for Each Month")
+plt.xlabel("Hour of the Day")
+plt.ylabel("Month")
+plt.savefig("pics/average_spread_by_hour_month.png", bbox_inches="tight")
+plt.show()
+
+# calculate the average spread per hour for each weekday
+avg_spread_by_hour_weekday = df.groupby(["weekday", "hour"])["difference da - id"].mean().unstack()
+
+# plot the average spread per hour for each weekday
+plt.figure(figsize=(15, 10))
+sns.heatmap(avg_spread_by_hour_weekday, annot=True, cmap="coolwarm", vmin=-1, vmax=1)
+plt.title("Average Spread by Hour for Each weekday")
+plt.xlabel("Hour of the Day")
+plt.ylabel("weekday")
+plt.savefig("pics/average_spread_by_hour_weekday.png", bbox_inches="tight")
+plt.show()
+
+# Calculate correlation between day ahead forecasted PV/Wind and the spread
+correlation_pv_spread = df["PV Day Ahead Forecast [in MWh]"].corr(df["difference da - id"])
+correlation_wind_spread = df["Wind Day Ahead Forecast [in MWh]"].corr(df["difference da - id"])
+
+print("Correlation between PV Day Ahead Forecast and Spread: ", correlation_pv_spread)
+print("Correlation between Wind Day Ahead Forecast and Spread: ", correlation_wind_spread)
+
+# Calculate correlation between day ahead forecast of PV and Wind against the Intraday values
+df["difference PV da - id"] = df["PV Day Ahead Forecast [in MWh]"] - df["PV Intraday Forecast [in MWh]"]
+df["difference Wind da - id"] = df["Wind Day Ahead Forecast [in MWh]"] - df["Wind Intraday Forecast [in MWh]"]
+
+correlation_dif_pv_spread = df["difference PV da - id"].corr(df["difference da - id"])
+correlation_dif_wind_spread = df["difference Wind da - id"].corr(df["difference da - id"])
+
+print("Correlation between difference in PV da - id and Spread: ", correlation_dif_pv_spread)
+print("Correlation between difference in Wind da - id and Spread: ", correlation_dif_wind_spread)
+
+# Calculate the cumulative sum of the strategy (price day ahead - intraday price) for 18 and 19 o'clock
+strategy_df = df[(df["hour"] == 17) | (df["hour"] == 18)]
+strategy_df["strategy_profit"] = (strategy_df["Day Ahead Price hourly [in EUR/MWh]"] - strategy_df["Intraday Price Hourly  [in EUR/MWh]"]) * 100
+strategy_df["cumulative_strategy_1_profit"] = strategy_df["strategy_profit"].cumsum()
+
+print(strategy_df.tail())
+
+# Plot the cumulative sum of the strategy
+plt.figure(figsize=(12, 6))
+plt.plot(strategy_df["time"], strategy_df["cumulative_strategy_1_profit"], label="Cumulative Strategy Profit [in EUR]")
+plt.xlabel("Time")
+plt.ylabel("Cumulative Profit [in EUR]")
+plt.title("Cumulative Sum of Strategy 1 (DA Price - ID Price) for 17 and 18 o\'clock")
+plt.legend()
+plt.savefig("pics/cumulative_strategy_1_profit.png", bbox_inches="tight")
+plt.show()
+
+# Calculate the cumulative sum of the strategy (price day ahead - intraday price) for 15-21 o'clock on friday, saturday and sunday
+strategy_2_df = df[((df["hour"] >= 15) & (df["hour"] <= 21)) & (df["weekday"].isin([4, 5, 6]))]
+strategy_2_df["strategy_profit"] = (strategy_2_df["Day Ahead Price hourly [in EUR/MWh]"] - strategy_2_df["Intraday Price Hourly  [in EUR/MWh]"]) * 100
+strategy_2_df["cumulative_strategy_2_profit"] = strategy_2_df["strategy_profit"].cumsum()
+
+print(strategy_2_df.tail())
+
+# Plot the cumulative sum of the strategy
+plt.figure(figsize=(12, 6))
+plt.plot(strategy_2_df["time"], strategy_2_df["cumulative_strategy_2_profit"], label="Cumulative Strategy Profit [in EUR]")
+plt.xlabel("Time")
+plt.ylabel("Cumulative Profit [in EUR]")
+plt.title("Cumulative Sum of Strategy 2 (DA Price - ID Price) for 15-21 o\'clock on Friday, Saturday and Sunday")
+plt.legend()
+plt.savefig("pics/cumulative_strategy_2_profit.png", bbox_inches="tight")
+plt.show()
